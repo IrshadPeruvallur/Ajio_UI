69a9ad4b0c4d483e39acca30259ef6614c18a48a
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2023-11-11 17:34:49 +0530

    update bag

--- lib/screen/bag_screen.dart
+++ lib/screen/bag_screen.dart
@@ -9,7 +9,7 @@ class BagScreen extends StatelessWidget {
     bagItems('asset/b9.png', 'Max', 'Men Slim Fit Polo', 350, 449, 22, 99, 1),
     bagItems('asset/b8.png', 'Max', 'Men Slim Fit Polo', 350, 449, 22, 99, 1),
     bagItems('asset/b9.png', 'Max', 'Men Slim Fit Polo', 350, 449, 22, 99, 1),
-    bagItems('asset/b9.png', 'Max', 'Men Slim Fit Polo', 350, 449, 22, 99, 1),
+    // bagItems('asset/b9.png', 'Max', 'Men Slim Fit Polo', 350, 449, 22, 99, 1),
   ];
 
   @override
@@ -39,32 +39,94 @@ class BagScreen extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Bag (Products)",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Bag (Products)",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Image(image: AssetImage('asset/d1.PNG')),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) => bagList[index],
+                    itemCount: bagList.length,
+                    separatorBuilder: (BuildContext context, int index) {
+                      return Divider();
+                    },
+                  ),
+                  Center(
+                      child: Text(
+                    'Assured Quality | 100% Handpicked | Easy Exchange',
+                    style: TextStyle(fontSize: 9),
+                  )),
+                ],
               ),
-              Image(image: AssetImage('asset/d1.PNG')),
-              ListView.separated(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) => bagList[index],
-                itemCount: bagList.length,
-                separatorBuilder: (BuildContext context, int index) {
-                  return Divider();
-                },
-              ),
-              Center(
+            ),
+            ListTile(
+              tileColor: Colors.white,
+              title: Text('Apply coupon'),
+              leading: Icon(Icons.redeem),
+              trailing: TextButton(
+                  onPressed: () {},
                   child: Text(
-                'Assured Quality | 100% Handpicked | Easy Exchange',
-                style: TextStyle(fontSize: 9),
-              ))
-            ],
-          ),
+                    'Select',
+                    style: TextStyle(color: Colors.blue),
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Order Payment Details',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Order Amount',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 11)),
+                            Text('Order Saving',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 11)),
+                            Text('Delivery Fee',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 11)),
+                            Text('Fulfiment Fee',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w300, fontSize: 11)),
+                            Text('Order Total'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('data'),
+                            Text('datadsasf'),
+                            Text('data'),
+                            Text('data'),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
