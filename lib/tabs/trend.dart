4e3160d8539e9419a9048f410b983b3be7ad08f9
@irshadpukayoor@gmail.com <irshadpukayoor@gmail.com>
2023-11-17 06:55:12 +0530

    modified trend

--- lib/tabs/trend.dart
+++ lib/tabs/trend.dart
@@ -15,19 +15,80 @@ class TrendTab extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image(image: AssetImage('asset/z1.jpg')),
-            Carousel(
-                carouselImage: sliderImage,
-                Height: 500,
-                width: double.infinity,
-                ViewportFraction: 1),
-            Row(
-              children: [],
-            )
-          ],
-        ),
+        child: Column(children: [
+          Image(image: AssetImage('asset/z1.jpg')),
+          Carousel(
+              carouselImage: sliderImage,
+              Height: 500,
+              width: double.infinity,
+              ViewportFraction: 1),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(
+                    height: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(image: AssetImage('asset/z6.jpg')),
+                    )),
+                SizedBox(
+                    height: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(image: AssetImage('asset/z7.jpg')),
+                    )),
+                SizedBox(
+                    height: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(image: AssetImage('asset/z8.jpg')),
+                    )),
+              ],
+            ),
+          ),
+          Image(image: AssetImage('asset/z11.jpg')),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(
+                    height: 300,
+                    child: Image(image: AssetImage('asset/z10.jpg'))),
+                SizedBox(
+                    height: 300,
+                    child: Image(image: AssetImage('asset/z9.jpg'))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                buildWishlistItem('asset/a4.png', 'Max',
+                    'Men Slim Fit Polo T-Shirt', 350, 449, 22),
+                SizedBox(
+                  width: 20,
+                ),
+                buildWishlistItem('asset/jewellry.jpg', 'Gold',
+                    'Women Gold Nechalise', 35000, 45000, 12),
+                SizedBox(
+                  width: 20,
+                ),
+                buildWishlistItem(
+                    'asset/b9.png', 'Watch', 'Man Watch', 4599, 5999, 25),
+                SizedBox(
+                  width: 20,
+                ),
+                buildWishlistItem('asset/a4.png', 'Max',
+                    'Men Slim Fit Polo T-Shirt', 350, 449, 22),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
